051e89e2b959ce89dce274be278ba39b2302814c
ungchun <leedool3003@gmail.com>
2022-12-04 19:39:51 +0900

    [chore] BaseViewController 수정

--- FindTown/FindTown/Presentation/Common/ViewController/BaseViewController.swift
+++ FindTown/FindTown/Presentation/Common/ViewController/BaseViewController.swift
@@ -4,33 +4,18 @@ import RxSwift
 class BaseViewController: UIViewController {
     var disposeBag = DisposeBag()
     
-    deinit {
-        disposeBag = DisposeBag()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addView()
+        setupView()
+        setLayout()
+        
         bindViewModel()
     }
     
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        setUp()
-        addView()
-        setLayout()
-        configureVC()
-        configureNavigation()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-   
-    func setUp() {}
     func addView() {}
+    func setupView() {}
     func setLayout() {}
-    func configureVC() {}
-    func configureNavigation() {}
     
     func bindViewModel() {}
 }
