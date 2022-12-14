b792abbd630560b27049e08c5c5255faf4981721
llghdud921 <llghdud921@naver.com>
2022-12-14 15:41:23 +0900

    [feat] UI, Core, Network package 추가 target platform 추가

---
+++ FindTown/FindTownNetwork/README.md
@@ -0,0 +1,3 @@
+# FindTownNetwork
+
+A description of this package.
---
+++ FindTown/FindTownNetwork/Sources/FindTownNetwork/FindTownNetwork.swift
@@ -0,0 +1,6 @@
+public struct FindTownNetwork {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
