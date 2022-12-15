40aae05d521fb801e00c48f0dec3eadfc5304a4e
ungchun <leedool3003@gmail.com>
2022-12-15 16:41:26 +0900

    [feat] RadioButton 추가 #9

--- FindTown/FindTownUI/Sources/FindTownUI/Button/RadioButton.swift
+++ FindTown/FindTownUI/Sources/FindTownUI/Button/RadioButton.swift
@@ -5,4 +5,25 @@
 //  Created by 김성훈 on 2022/12/15.
 //
 
-import Foundation
+import UIKit
+
+public final class RadioButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        let image = UIImage(systemName: "circle")
+        let selectedImage = UIImage(systemName: "circle.circle.fill")
+        
+        setImage(image, for: .normal)
+        setImage(selectedImage, for: .selected)
+    }
+}
