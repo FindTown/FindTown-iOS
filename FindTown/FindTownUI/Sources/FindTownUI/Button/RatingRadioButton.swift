ea8f19168bd44a3ca98d549da64dc8bb5d6b3351
ungchun <leedool3003@gmail.com>
2022-12-15 16:42:28 +0900

    [feat] RatingRadioButton 추가 #9

--- FindTown/FindTownUI/Sources/FindTownUI/Button/RatingRadioButton.swift
+++ FindTown/FindTownUI/Sources/FindTownUI/Button/RatingRadioButton.swift
@@ -5,4 +5,130 @@
 //  Created by 김성훈 on 2022/12/15.
 //
 
-import Foundation
+import UIKit
+
+public final class RatingRadioButton: UIView {
+    
+    public enum StarType {
+        case none
+        case three
+        case five
+    }
+    
+    private var starType: StarType
+    private var labelPadding: Double = 10.0
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    public let radioButton: RadioButton = {
+        let btn = RadioButton()
+        btn.isUserInteractionEnabled = false
+        return btn
+    }()
+    
+    private lazy var starStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        stackView.isUserInteractionEnabled = false
+        return stackView
+    }()
+    
+    private let star1: StarButton = {
+        let btn = StarButton()
+        btn.isUserInteractionEnabled = false
+        return btn
+    }()
+    
+    private let star2: StarButton = {
+        let btn = StarButton()
+        btn.isUserInteractionEnabled = false
+        return btn
+    }()
+    
+    private let star3: StarButton = {
+        let btn = StarButton()
+        btn.isUserInteractionEnabled = false
+        return btn
+    }()
+    
+    private let star4: StarButton = {
+        let btn = StarButton()
+        btn.isUserInteractionEnabled = false
+        return btn
+    }()
+    
+    private let star5: StarButton = {
+        let btn = StarButton()
+        btn.isUserInteractionEnabled = false
+        return btn
+    }()
+    
+    private let rattingGuirdTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        return label
+    }()
+    
+    public init(starType: StarType) {
+        self.starType = starType
+        super.init(frame: CGRect.zero)
+        
+        addView()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addView() {
+        starStackView.addArrangedSubview(star1)
+        starStackView.addArrangedSubview(star2)
+        starStackView.addArrangedSubview(star3)
+        starStackView.addArrangedSubview(star4)
+        starStackView.addArrangedSubview(star5)
+        
+        backgroundView.addSubview(radioButton)
+        backgroundView.addSubview(starStackView)
+        backgroundView.addSubview(rattingGuirdTitle)
+        
+        switch starType {
+        case .none:
+            [star1, star2, star3, star4, star5].forEach {
+                $0.isHidden = true
+            }
+            labelPadding = 0
+            rattingGuirdTitle.text = "크게 중요하지 않아요."
+        case .three:
+            [star1, star2, star3].forEach {
+                $0.isSelected = true
+            }
+            rattingGuirdTitle.text = "중요해요."
+        case .five:
+            [star1, star2, star3, star4, star5].forEach {
+                $0.isSelected = true
+            }
+            rattingGuirdTitle.text = "매우 중요해요."
+        }
+        
+        addSubview(backgroundView)
+    }
+    
+    private func setLayout() {
+        radioButton.leftAnchor.constraint(equalTo: super.leftAnchor).isActive = true
+        radioButton.centerYAnchor.constraint(equalTo: super.centerYAnchor).isActive = true
+        
+        starStackView.leftAnchor.constraint(equalTo: radioButton.rightAnchor, constant: 10).isActive = true
+        starStackView.centerYAnchor.constraint(equalTo: super.centerYAnchor).isActive = true
+        
+        rattingGuirdTitle.leftAnchor.constraint(equalTo: starStackView.rightAnchor, constant: labelPadding).isActive = true
+        rattingGuirdTitle.centerYAnchor.constraint(equalTo: super.centerYAnchor).isActive = true
+    }
+}
