138c3ba7b56a26f30d5d4cdf2e3f099c97d96ee2
ungchun <leedool3003@gmail.com>
2022-12-15 16:41:01 +0900

    [feat] CheckButton 추가 #9

--- FindTown/FindTownUI/Sources/FindTownUI/Button/CheckButton.swift
+++ FindTown/FindTownUI/Sources/FindTownUI/Button/CheckButton.swift
@@ -5,4 +5,26 @@
 //  Created by 김성훈 on 2022/12/15.
 //
 
-import Foundation
+import UIKit
+
+public final class CheckButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        let image = UIImage(systemName: "checkmark.circle")
+        let selectedImage = UIImage(systemName: "checkmark.circle.fill")
+        
+        setImage(image, for: .normal)
+        setImage(selectedImage, for: .selected)
+    }
+}
