15327475ae2f2bcd20b5f4687dc28371a5b22ab2
seonyoung <4tjsdud2@naver.com>
2022-12-16 16:15:55 +0900

    [feat] Color 추가 #9

---
+++ FindTown/FindTownUI/Sources/FindTownUI/UIColor+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by 장선영 on 2022/12/16.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let back1: UIColor = .init(named: "Back1") ?? .clear
+    static let back2: UIColor = .init(named: "Back2") ?? .clear
+    static let back3: UIColor = .init(named: "Back3") ?? .clear
+    
+    static let black: UIColor = .init(named: "Black") ?? .clear
+    static let dim20: UIColor = .init(named: "Dim20") ?? .clear
+    static let dim50: UIColor = .init(named: "Dim50") ?? .clear
+    static let dim70: UIColor = .init(named: "Dim70") ?? .clear
+    
+    static let grey1: UIColor = .init(named: "Grey1") ?? .clear
+    static let grey2: UIColor = .init(named: "Grey2") ?? .clear
+    static let grey3: UIColor = .init(named: "Grey3") ?? .clear
+    static let grey4: UIColor = .init(named: "Grey4") ?? .clear
+    static let grey5: UIColor = .init(named: "Grey5") ?? .clear
+    static let grey6: UIColor = .init(named: "Grey6") ?? .clear
+    static let grey7: UIColor = .init(named: "Grey7") ?? .clear
+    
+    static let primary: UIColor = .init(named: "Primary") ?? .clear
+    static let secondary: UIColor = .init(named: "Secondary") ?? .clear
+    static let white: UIColor = .init(named: "White") ?? .clear
+    
+}
