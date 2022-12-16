fc9e497cfff90bcb90efe8908fc3d807d56a5340
seonyoung <4tjsdud2@naver.com>
2022-12-16 17:31:07 +0900

    [feat] Color 적용 수정 #9

--- FindTown/FindTownUI/Package.swift
+++ FindTown/FindTownUI/Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FindTownUI",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Assets.xcassets")]),
         .testTarget(
             name: "FindTownUITests",
             dependencies: ["FindTownUI"]),
--- FindTown/FindTownUI/Sources/FindTownUI/UIColor+Extension.swift
+++ FindTown/FindTownUI/Sources/FindTownUI/UIColor+Extension.swift
@@ -8,27 +8,27 @@
 import Foundation
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     
-    static let back1: UIColor = .init(named: "Back1") ?? .clear
-    static let back2: UIColor = .init(named: "Back2") ?? .clear
-    static let back3: UIColor = .init(named: "Back3") ?? .clear
+    static let back1: UIColor = .init(named: "Back1", in: .module, compatibleWith: nil) ?? .clear
+    static let back2: UIColor = .init(named: "Back2", in: .module, compatibleWith: nil) ?? .clear
+    static let back3: UIColor = .init(named: "Back3", in: .module, compatibleWith: nil) ?? .clear
     
-    static let black: UIColor = .init(named: "Black") ?? .clear
-    static let dim20: UIColor = .init(named: "Dim20") ?? .clear
-    static let dim50: UIColor = .init(named: "Dim50") ?? .clear
-    static let dim70: UIColor = .init(named: "Dim70") ?? .clear
+    static let black: UIColor = .init(named: "Black", in: .module, compatibleWith: nil) ?? .clear
+    static let dim20: UIColor = .init(named: "Dim20", in: .module, compatibleWith: nil) ?? .clear
+    static let dim50: UIColor = .init(named: "Dim50", in: .module, compatibleWith: nil) ?? .clear
+    static let dim70: UIColor = .init(named: "Dim70", in: .module, compatibleWith: nil) ?? .clear
     
-    static let grey1: UIColor = .init(named: "Grey1") ?? .clear
-    static let grey2: UIColor = .init(named: "Grey2") ?? .clear
-    static let grey3: UIColor = .init(named: "Grey3") ?? .clear
-    static let grey4: UIColor = .init(named: "Grey4") ?? .clear
-    static let grey5: UIColor = .init(named: "Grey5") ?? .clear
-    static let grey6: UIColor = .init(named: "Grey6") ?? .clear
-    static let grey7: UIColor = .init(named: "Grey7") ?? .clear
+    static let grey1: UIColor = .init(named: "Grey1", in: .module, compatibleWith: nil) ?? .clear
+    static let grey2: UIColor = .init(named: "Grey2", in: .module, compatibleWith: nil) ?? .clear
+    static let grey3: UIColor = .init(named: "Grey3", in: .module, compatibleWith: nil) ?? .clear
+    static let grey4: UIColor = .init(named: "Grey4", in: .module, compatibleWith: nil) ?? .clear
+    static let grey5: UIColor = .init(named: "Grey5", in: .module, compatibleWith: nil) ?? .clear
+    static let grey6: UIColor = .init(named: "Grey6", in: .module, compatibleWith: nil) ?? .clear
+    static let grey7: UIColor = .init(named: "Grey7", in: .module, compatibleWith: nil) ?? .clear
     
-    static let primary: UIColor = .init(named: "Primary") ?? .clear
-    static let secondary: UIColor = .init(named: "Secondary") ?? .clear
-    static let white: UIColor = .init(named: "White") ?? .clear
+    static let primary: UIColor = .init(named: "Primary", in: .module, compatibleWith: nil) ?? .clear
+    static let secondary: UIColor = .init(named: "Secondary", in: .module, compatibleWith: nil) ?? .clear
+    static let white: UIColor = .init(named: "White", in: .module, compatibleWith: nil) ?? .clear
     
 }
