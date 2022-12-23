a826a10891156c385a3a064b86c62f6c8d336097
llghdud921 <llghdud921@naver.com>
2022-12-23 14:49:15 +0900

    [feat] FindTownCore내 FindTownUI package dependency 추가

--- FindTown/FindTownCore/Package.swift
+++ FindTown/FindTownCore/Package.swift
@@ -17,14 +17,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift", branch: "main")
+        .package(url: "https://github.com/ReactiveX/RxSwift", branch: "main"),
+        .package(path: "../FindTownUI")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FindTownCore",
-            dependencies: ["RxSwift"]),
+            dependencies: ["RxSwift","FindTownUI"]),
         .testTarget(
             name: "FindTownCoreTests",
             dependencies: ["FindTownCore"]),
