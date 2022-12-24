c4c0d3e127f64a0800b1d63a738694e1f4f4d42b
llghdud921 <llghdud921@naver.com>
2022-12-24 19:42:18 +0900

    [feat] baseViewController 내 속성 수정 #17

--- FindTown/FindTownCore/Sources/FindTownCore/Base/BaseNavigationController.swift
+++ FindTown/FindTownCore/Sources/FindTownCore/Base/BaseNavigationController.swift
@@ -5,13 +5,12 @@
 //  Created by 이호영 on 2022/12/23.
 //
 
-import Foundation
 import UIKit
 import FindTownUI
 
-class BaseNavigationController: UINavigationController {
+public class BaseNavigationController: UINavigationController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
@@ -22,12 +21,12 @@ class BaseNavigationController: UINavigationController {
         let backButtonAppearance = UIBarButtonItemAppearance(style: .plain)
         backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
         
-        let backButtonImage = UIImage(named: "chevron.backward")
+        let backButtonImage = UIImage(systemName: "chevron.backward")
         backButtonImage?.withTintColor(FindTownColor.grey7.color)
         
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = UIColor.clear
+        appearance.backgroundColor = FindTownColor.white.color
         appearance.shadowColor = UIColor.clear
         appearance.backgroundEffect = UIBlurEffect(style: .light)
         appearance.backButtonAppearance = backButtonAppearance
