f7e346bde159c6c5a53cbbbc8b053f93b2d32cf0
llghdud921 <llghdud921@naver.com>
2022-12-24 19:42:39 +0900

    [feat] BaseTabBarController 추가 #17

---
+++ FindTown/FindTownCore/Sources/FindTownCore/Base/BaseTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이호영 on 2022/12/23.
+//
+
+import UIKit
+import FindTownUI
+
+public class BaseTabBarController: UITabBarController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTabBarAppearance()
+    }
+    
+    func setTabBarAppearance() {
+        let itemAppearance = UITabBarItemAppearance()
+        itemAppearance.normal.iconColor = FindTownColor.grey6.color
+        itemAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: FindTownColor.grey6.color]
+        
+        itemAppearance.selected.iconColor = FindTownColor.primary.color
+        itemAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: FindTownColor.primary.color]
+        
+        let appearance = UITabBarAppearance()
+        
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundEffect = .none
+        appearance.backgroundColor = FindTownColor.white.color
+        appearance.shadowColor = FindTownColor.white.color
+        
+        appearance.stackedLayoutAppearance = itemAppearance
+        appearance.inlineLayoutAppearance = itemAppearance
+        appearance.compactInlineLayoutAppearance = itemAppearance
+        
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.standardAppearance = appearance
+    }
+}
