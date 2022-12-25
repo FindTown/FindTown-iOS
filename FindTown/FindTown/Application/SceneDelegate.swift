c9429fc84962b3a3e1b78d2dc27e3118ac1c4275
llghdud921 <llghdud921@naver.com>
2022-12-25 14:03:27 +0900

    [feat] SceneDelegate내 AppCoordinator 실행 로직 구현 #18

--- FindTown/FindTown/Application/SceneDelegate.swift
+++ FindTown/FindTown/Application/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FindTownCore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -18,9 +19,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainViewController = ViewController()
         
-        window?.rootViewController = mainViewController
+        let navigationController = BaseNavigationController()
+        AppCoordinator(navigationController: navigationController).start()
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
     
