a237be04a7fd483c7845f76eef2692d7196f4c89
llghdud921 <llghdud921@naver.com>
2022-12-25 14:02:33 +0900

    [feat] TabBarCoordinator 구현 #18

---
+++ FindTown/FindTown/Presentation/Common/Flow/TabBarCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarCoordinator.swift
+//  FindTown
+//
+//  Created by 이호영 on 2022/12/25.
+//
+
+import UIKit
+import FindTownCore
+
+final class TabBarCoordinator: FlowCoordinator {
+    
+    var presentationStyle: PresentationStyle
+    weak var navigationController: UINavigationController?
+    
+    
+    init(presentationStyle: PresentationStyle) {
+        self.presentationStyle = presentationStyle
+    }
+    
+    func initScene() -> UIViewController {
+        let tabBarController: UITabBarController = BaseTabBarController()
+        
+        let homeCoordinator = HomeCoordinator(presentationStyle: .none)
+        homeCoordinator.start()
+        guard let homeViewController = homeCoordinator.navigationController else { return UIViewController() }
+        homeViewController.tabBarItem = UITabBarItem(title: "동네 찾기", image: UIImage(systemName: "house"), tag: 0)
+        
+        let mapCoordinator = MapCoordinator(presentationStyle: .none)
+        mapCoordinator.start()
+        guard let mapViewController = mapCoordinator.navigationController else { return UIViewController() }
+        mapViewController.tabBarItem = UITabBarItem(title: "동네 지도", image: UIImage(systemName: "map"), tag: 1)
+        
+        tabBarController.viewControllers = [homeViewController, mapViewController]
+        tabBarController.hidesBottomBarWhenPushed = true
+        return tabBarController
+    }
+}
