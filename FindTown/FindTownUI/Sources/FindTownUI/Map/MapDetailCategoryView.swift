b082256c924269f587828ecf41e33c90d657b7b0
seonyoung <4tjsdud2@naver.com>
2023-01-02 23:59:59 +0900

    [feat] MapDetailCategoryView 구현 #23
    - 카테고리를 눌렀을 때 나오는 상세 카테고리 뷰 
    - Map에서 사용되는 UI Component들 Map 폴더로 분리

---
+++ FindTown/FindTownUI/Sources/FindTownUI/Map/MapDetailCategoryView.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  
+//
+//  Created by 장선영 on 2023/01/02.
+//
+
+import Foundation
+import UIKit
+
+public class MapDetailCategoryView: UIView {
+    
+    public var detailCategoryStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .clear
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MapDetailCategoryView {
+    
+    func setupView() {
+        self.backgroundColor = FindTownColor.white.color.withAlphaComponent(0.8)
+        self.layer.cornerRadius = 8
+    }
+    
+    func setupLayout() {
+        self.addSubview(detailCategoryStackView)
+        detailCategoryStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            detailCategoryStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8.0),
+            detailCategoryStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8.0),
+            detailCategoryStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8.0),
+            detailCategoryStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8.0)
+        ])
+    }
+}
