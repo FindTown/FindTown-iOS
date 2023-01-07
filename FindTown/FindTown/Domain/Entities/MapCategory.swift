ebcdb814d990044b6c3b91383d396d3dbc4bdfb7
llghdud921 <llghdud921@naver.com>
2023-01-07 15:17:00 +0900

    [file] Map data에 있는 model type들 domain내 Entity로 위치 변경
    - FindTownUI에 있던 MapDetailCategoryView, MapDetailComponentView Map내 subviews로 변경
    - MapViewController에 있었던 MapDetailCategory 관련 로직 View내부로 변경
    - AddressSheet 폴더 생성

---
+++ FindTown/FindTown/Domain/Entities/MapCategory.swift
@@ -0,0 +1,14 @@
+//
+//  MapData.swift
+//  FindTown
+//
+//  Created by 장선영 on 2023/01/05.
+//
+
+import UIKit
+
+/// 임시로 설계해놓은 Map Test Data
+enum MapCategory {
+    case infra
+    case theme
+}
---
+++ FindTown/FindTown/Domain/Entities/MapData.swift
@@ -0,0 +1,12 @@
+//
+//  MapCategory.swift
+//  FindTown
+//
+//  Created by 이호영 on 2023/01/07.
+//
+
+import Foundation
+
+struct MapData {
+    let categories: [Category]
+}
