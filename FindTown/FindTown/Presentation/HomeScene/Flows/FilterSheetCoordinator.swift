3c494e7bdfab0182b7afc3ec0b0a7f26bae07463
ungchun <leedool3003@gmail.com>
2023-01-17 16:34:47 +0900

    [chore] FilterSheetCoordinator set #37

---
+++ FindTown/FindTown/Presentation/HomeScene/Flows/FilterSheetCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  FilterSheetCoordinator.swift
+//  FindTown
+//
+//  Created by 김성훈 on 2023/01/16.
+//
+
+import UIKit
+
+import FindTownCore
+
+final class FilterSheetCoordinator: FlowCoordinator {
+    
+    var presentationStyle: PresentationStyle
+    weak var navigationController: UINavigationController?
+    
+    init(presentationStyle: PresentationStyle) {
+        self.presentationStyle = presentationStyle
+    }
+    
+    internal func initScene() -> UIViewController {
+        let addressSheetViewModel = FilterBottomSheetViewModel(delegate: self)
+        return FilterBottonSheetViewController(viewModel: addressSheetViewModel)
+    }
+}
+
+extension FilterSheetCoordinator: FilterBottomSheetViewModelDelegate {
+    func dismiss(_ tempModel: String) {
+        guard let navigationController = navigationController else { return }
+        if let homeVC = navigationController.viewControllers.first as? HomeViewController {
+            homeVC.dismissBottomSheet()
+        }
+        navigationController.dismiss(animated: true)
+    }
+}
--- FindTown/FindTown/Presentation/HomeScene/Flows/HomeCoordinator.swift
+++ FindTown/FindTown/Presentation/HomeScene/Flows/HomeCoordinator.swift
@@ -22,34 +22,13 @@ final class HomeCoordinator: FlowCoordinator {
         let homeViewController = HomeViewController(viewModel: homeViewModel)
         return homeViewController
     }
-    
 }
 
 extension HomeCoordinator: HomeViewModelDelegate {
     
-    func push() {
+    func goToFilterBottomSheet() {
         guard let navigationController = navigationController else { return }
-        MapCoordinator(presentationStyle: .push(navigationController: navigationController)).start()
+        FilterSheetCoordinator(presentationStyle: .present(navigationController: navigationController,
+                                                           modalPresentationStyle: .overFullScreen)).start()
     }
-    
-    func present() {
-        guard let navigationController = navigationController else { return }
-        HomeCoordinator(presentationStyle: .present(navigationController: navigationController)).start()
-    }
-    
-    func pop() {
-        guard let navigationController = navigationController else { return }
-        navigationController.popViewController(animated: true)
-    }
-    
-    func dismiss() {
-        guard let navigationController = navigationController else { return }
-        navigationController.dismiss(animated: true)
-    }
-    
-    func setViewController() {
-        guard let navigationController = navigationController else { return }
-        TabBarCoordinator(presentationStyle: .setViewController(navigationController: navigationController)).start()
-    }
-    
 }
