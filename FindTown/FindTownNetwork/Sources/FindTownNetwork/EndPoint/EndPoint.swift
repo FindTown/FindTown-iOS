a10f95b84cd16350af91792090d1d0f9d4acf3f1
llghdud921 <llghdud921@naver.com>
2023-01-22 00:24:52 +0900

    [feat] Endpoint 구현 #46

---
+++ FindTown/FindTownNetwork/Sources/FindTownNetwork/EndPoint/EndPoint.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이호영 on 2023/01/22.
+//
+
+import Foundation
+
+class EndPoint<Request: RequestType> {
+    
+    let session: Session
+    
+    init(session: Session) {
+        self.session = session
+    }
+    
+    func request(target: Request, cachePolicy: URLRequest.CachePolicy) async throws -> Data {
+        let url = URL(target: target)
+        var request = URLRequest(url: url, cachePolicy: cachePolicy)
+        request.httpMethod = target.method.value
+        
+        for header in target.headers.headers {
+            request.setValue(header.value, forHTTPHeaderField: header.name.description)
+        }
+        
+        switch target.task {
+        case .requestPlain:
+            return try await session.request(request: request)
+        case .requestData(let data):
+            request.httpBody = data
+            return try await session.request(request: request)
+        case .requestJSONEncodable(let encodable):
+            return try await session.request(request: request.encoded(encodable: encodable))
+        case .requestCustomJSONEncodable(let encodable, let encoder):
+            return try await session.request(request: request.encoded(encodable: encodable, encoder: encoder))
+        }
+    }
+}
