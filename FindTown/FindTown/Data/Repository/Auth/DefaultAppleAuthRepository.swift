e874940e4d5f7d469d76ae4b08d7ffba22d9377d
llghdud921 <llghdud921@naver.com>
2023-01-25 21:56:20 +0900

    [feat] DefaultAppleAuthRepository 추가 #42

---
+++ FindTown/FindTown/Data/Repository/Auth/DefaultAppleAuthRepository.swift
@@ -0,0 +1,67 @@
+//
+//  DefaultAppleAuthRepository.swift
+//  FindTown
+//
+//  Created by 이호영 on 2023/01/25.
+//
+
+import Foundation
+import AuthenticationServices
+import FindTownNetwork
+
+final class DefaultAppleAuthRespository: NSObject {
+    
+    private var authcontinuation: CheckedContinuation<String, Error>?
+    
+    lazy var authorizationController: ASAuthorizationController = {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        return authorizationController
+    }()
+     
+    func loginWithApple() async throws -> String {
+        return try await withCheckedThrowingContinuation { continuation in
+            authcontinuation = continuation
+            authorizationController.performRequests()
+        }
+    }
+    
+    func checkAutoSign(userId: String) async throws -> Bool {
+        return try await withCheckedThrowingContinuation { continuation in
+            let appleIDProvider = ASAuthorizationAppleIDProvider()
+            appleIDProvider.getCredentialState(forUserID: userId) { credentialState, error in
+                switch credentialState {
+                  case .authorized:
+                    continuation.resume(returning: true)
+                    break
+                  case .revoked:
+                    continuation.resume(returning: false)
+                    break
+                  case .notFound:
+                    continuation.resume(returning: false)
+                    break
+                  default:
+                    continuation.resume(returning: false)
+                    break
+                  }
+            }
+        }
+    }
+}
+
+extension DefaultAppleAuthRespository: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            authcontinuation?.resume(returning: appleIDCredential.user)
+        } else {
+            authcontinuation?.resume(throwing: FTNetworkError.apple)
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        authcontinuation?.resume(throwing: error)
+    }
+}
