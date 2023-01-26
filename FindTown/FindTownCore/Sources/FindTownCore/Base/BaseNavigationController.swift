2f5c3a180915e8d5de69e47c60466ae7a466138b
ungchun <leedool3003@gmail.com>
2023-01-26 14:16:29 +0900

    [chore] FindTownUI Bundle Core에서 사용할 수 있게 extension 추가 #41

--- FindTown/FindTownCore/Sources/FindTownCore/Base/BaseNavigationController.swift
+++ FindTown/FindTownCore/Sources/FindTownCore/Base/BaseNavigationController.swift
@@ -21,8 +21,7 @@ public class BaseNavigationController: UINavigationController {
         let backButtonAppearance = UIBarButtonItemAppearance(style: .plain)
         backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
         
-        let backButtonImage = UIImage(named: "Back", in: .module, compatibleWith: nil)?.withAlignmentRectInsets(UIEdgeInsets(top: 0.0, left: -8.0, bottom: -8.0, right: 0.0))
-        backButtonImage?.withTintColor(FindTownColor.grey7.color)
+        let backButtonImage = UIImage(named: "Back", in: .FindTownUI, compatibleWith: nil)?.withAlignmentRectInsets(UIEdgeInsets(top: 0.0, left: -8.0, bottom: -8.0, right: 0.0))
         
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
