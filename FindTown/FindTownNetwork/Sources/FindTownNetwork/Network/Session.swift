8860f5cdd5f6b6350180276bb2f823ae619bab11
llghdud921 <llghdud921@naver.com>
2023-01-27 21:40:33 +0900

    [feat] log 로직 적용

--- FindTown/FindTownNetwork/Sources/FindTownNetwork/Network/Session.swift
+++ FindTown/FindTownNetwork/Sources/FindTownNetwork/Network/Session.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import FindTownCore
 
 protocol Sessionable {
     func request(request: URLRequest) async throws -> Data
@@ -19,10 +20,12 @@ class Session: Sessionable {
     }
     
     func request(request: URLRequest) async throws -> Data {
+        self.log(request: request)
         let (data, response) = try await self.session.data(for: request, delegate: nil)
         guard let httpResponse = response as? HTTPURLResponse else {
             throw FTNetworkError.invaildServerResponse
         }
+        self.log(response: httpResponse)
         
         if 401 == httpResponse.statusCode {
             throw FTNetworkError.unauthorized
@@ -35,5 +38,17 @@ class Session: Sessionable {
             throw FTNetworkError.server
         }
         return data
+    }
+    
+    func log(request: URLRequest) {
+        Log.debug("reqeust 👇", ["url : \(String(describing: request.url))",
+                                   "method : \(String(describing: request.httpMethod))",
+                                   "header : \(String(describing: request.allHTTPHeaderFields))",
+                                   "body : \(String(describing: request.httpBody)) "])
+    }
+    
+    func log(response: HTTPURLResponse) {
+        Log.debug("reponse 👇", ["statusCode : \(String(describing: response.statusCode ))",
+                                 "description : \(String(describing: response.description ))"])
     }
 }
