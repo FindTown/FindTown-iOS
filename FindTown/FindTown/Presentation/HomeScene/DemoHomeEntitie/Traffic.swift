76a0648592fd4098a7a0e7fdacbb5e0d301ac47f
seonyoung <4tjsdud2@naver.com>
2023-01-30 13:45:47 +0900

    [refactor] Traffic 타입에 color 추가 #56

--- FindTown/FindTown/Presentation/HomeScene/DemoHomeEntitie/Traffic.swift
+++ FindTown/FindTown/Presentation/HomeScene/DemoHomeEntitie/Traffic.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum Traffic: CaseIterable {
     case one
@@ -93,6 +94,43 @@ enum Traffic: CaseIterable {
             return "우이신설"
         }
     }
+    
+    var color: UIColor {
+        switch self {
+        case .one:
+            return UIColor(red: 38, green: 60, blue: 150)
+        case .two:
+            return UIColor(red: 60, green: 180, blue: 74)
+        case .three:
+            return UIColor(red: 255, green: 115, blue: 0)
+        case .four:
+            return UIColor(red: 44, green: 158, blue: 222)
+        case .five:
+            return UIColor(red: 137, green: 54, blue: 224)
+        case .six:
+            return UIColor(red: 181, green: 80, blue: 11)
+        case .seven:
+            return UIColor(red: 105, green: 114, blue: 21)
+        case .eight:
+            return UIColor(red: 229, green: 30, blue: 110)
+        case .nine:
+            return UIColor(red: 209, green: 166, blue: 44)
+        case .gyeonguijungang:
+            return UIColor(red: 124, green: 196, blue: 165)
+        case .gonghangcheoldo:
+            return UIColor(red: 115, green: 182, blue: 228)
+        case .suinbundang:
+            return UIColor(red: 255, green: 206, blue: 51)
+        case .sinrimseon:
+            return UIColor(red: 125, green: 154, blue: 223)
+        case .sinbundang:
+            return UIColor(red: 167, green: 30, blue: 49)
+        case .uisinseol:
+            return UIColor(red: 179, green: 199, blue: 62)
+        }
+    }
+    
+    
     
     static func returnTrafficType(_ traffic: String) -> Traffic? {
         return self.allCases.first { $0.shortDescription == traffic }
--- FindTown/FindTown/Presentation/TownIntroScene/TownIntro/SubViews/TrafficTip/TrafficTipView.swift
+++ FindTown/FindTown/Presentation/TownIntroScene/TownIntro/SubViews/TrafficTip/TrafficTipView.swift
@@ -18,39 +18,7 @@ final class TrafficTipView: UIView {
     convenience init(type: Traffic) {
         self.init(frame: .zero)
         trafficLabel.text = type.shortDescription
-        
-        switch type {
-        case .one:
-            self.backgroundColor = UIColor(red: 38, green: 60, blue: 150)
-        case .two:
-            self.backgroundColor = UIColor(red: 60, green: 180, blue: 74)
-        case .three:
-            self.backgroundColor = UIColor(red: 255, green: 115, blue: 0)
-        case .four:
-            self.backgroundColor = UIColor(red: 44, green: 158, blue: 222)
-        case .five:
-            self.backgroundColor = UIColor(red: 137, green: 54, blue: 224)
-        case .six:
-            self.backgroundColor = UIColor(red: 181, green: 80, blue: 11)
-        case .seven:
-            self.backgroundColor = UIColor(red: 105, green: 114, blue: 21)
-        case .eight:
-            self.backgroundColor = UIColor(red: 229, green: 30, blue: 110)
-        case .nine:
-            self.backgroundColor = UIColor(red: 209, green: 166, blue: 44)
-        case .gyeonguijungang:
-            self.backgroundColor = UIColor(red: 124, green: 196, blue: 165)
-        case .gonghangcheoldo:
-            self.backgroundColor = UIColor(red: 115, green: 182, blue: 228)
-        case .suinbundang:
-            self.backgroundColor = UIColor(red: 255, green: 206, blue: 51)
-        case .sinrimseon:
-            self.backgroundColor = UIColor(red: 125, green: 154, blue: 223)
-        case .sinbundang:
-            self.backgroundColor = UIColor(red: 167, green: 30, blue: 49)
-        case .uisinseol:
-            self.backgroundColor = UIColor(red: 179, green: 199, blue: 62)
-        }
+        self.backgroundColor = type.color
     }
     
     required init?(coder: NSCoder) {
