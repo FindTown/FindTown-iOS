05e77bc0dfdc47e1bb2a950d3c95376ad339f08b
ungchun <leedool3003@gmail.com>
2023-02-15 23:40:48 +0900

    [refactor] TownTableMockData 제거 #37

--- FindTown/FindTown/Domain/Entities/TownTableModel.swift
+++ FindTown/FindTown/Domain/Entities/TownTableModel.swift
@@ -15,12 +15,3 @@ struct TownTableModel {
     let safetyRate: Double
     let townIntroduction: String
 }
-
-func TownTableMockData() -> TownTableModel {
-    return TownTableModel(objectId: 321,
-                          county: "행운동",
-                          countyIcon: UIImage(named: "gwanak") ?? UIImage(),
-                          wishTown: false,
-                          safetyRate: 3,
-                          townIntroduction: "강남으로 출근하기 좋은 동네")
-}
