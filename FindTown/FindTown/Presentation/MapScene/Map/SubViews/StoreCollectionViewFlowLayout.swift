650fd20a1bc84f9c0d1076c6e7b769b9c7480c35
llghdud921 <llghdud921@naver.com>
2023-02-19 02:43:33 +0900

    [refactor] StoreCollectionFlowLayout 내 적용하였던 로직 제거 #24

--- FindTown/FindTown/Presentation/MapScene/Map/SubViews/StoreCollectionViewFlowLayout.swift
+++ FindTown/FindTown/Presentation/MapScene/Map/SubViews/StoreCollectionViewFlowLayout.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 final class StoreCollectionViewFlowLayout: UICollectionViewFlowLayout {
-
-    private var previousOffset: CGFloat = 0
-    private var currentPage: Int = 0
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -28,21 +25,4 @@ final class StoreCollectionViewFlowLayout: UICollectionViewFlowLayout {
         itemSize = CGSize(width: 290, height: 155)
     }
     
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        guard let collectionView = collectionView else {
-            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
-        }
-
-        let itemsCount = collectionView.numberOfItems(inSection: 0)
-        
-        if previousOffset > collectionView.contentOffset.x && velocity.x < 0 {
-            currentPage = max(currentPage - 1, 0)
-        } else if previousOffset < collectionView.contentOffset.x && velocity.x > 0 {
-            currentPage = min(currentPage + 1, itemsCount - 1)
-        }
-        
-        let updatedOffset = (itemSize.width + minimumLineSpacing) * CGFloat(currentPage)
-        previousOffset = updatedOffset
-        return CGPoint(x: updatedOffset, y: proposedContentOffset.y)
-    }
 }
