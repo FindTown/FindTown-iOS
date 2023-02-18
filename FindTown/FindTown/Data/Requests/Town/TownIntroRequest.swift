fea1ac756bdaae12f24f03dc50fc5084b37aadab
seonyoung <4tjsdud2@naver.com>
2023-02-18 10:46:51 +0900

    [feat] TownIntroRequest 생성 #79

---
+++ FindTown/FindTown/Data/Requests/Town/TownIntroRequest.swift
@@ -0,0 +1,25 @@
+//
+//  TownIntroRequest.swift
+//  FindTown
+//
+//  Created by 장선영 on 2023/02/17.
+//
+
+import Foundation
+import FindTownNetwork
+
+struct TownIntroRequest: BaseRequest {    
+    
+    init(HTTPHeaders: HTTPHeaders, parameters: [URLQueryItem]) {
+        self.headers = HTTPHeaders
+        self.parameters = parameters
+    }
+    
+    typealias ResponseType = TownIntroReponseDTO
+    
+    var path: String = "/app/town/introduce"
+    var method: FindTownNetwork.HttpMethod = .get
+    var headers: FindTownNetwork.HTTPHeaders
+    var task: FindTownNetwork.TaskType = .requestPlain
+    var parameters: [URLQueryItem]?
+}
